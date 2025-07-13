e89db5b65c362ee999547aaa39296c630826cb49
Lucas Colley <lucas.colley8@gmail.com>
2025-07-13 14:58:50 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,6 @@ install -m0644 crates/zed/resources/app-icon.png "$PREFIX/Menu/zed.png"
 
 if [[ "$target_platform" == "osx-arm64" && "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_PREFIX_PATH=${PREFIX}"
-  export CARGO_BUILD_TARGET=aarch64-apple-darwin
 fi
 
 # Set Cargo build profile
